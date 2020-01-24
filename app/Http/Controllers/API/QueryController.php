567886c0bf81b485a67f6abc7d8062a6ba4353e1
4M1LK4R <amilkar.dominguez@gmail.com>
2020-01-24 02:17:36 -0400

    Bug Consulta

--- app/Http/Controllers/API/QueryController.php
+++ app/Http/Controllers/API/QueryController.php
@@ -17,62 +17,32 @@ class QueryController extends Controller
 
     public function find(Request $request)
     {
-
         if (Product::where('state', 'ACTIVO')->where('barcode',  $request->barcode)->exists()) {
-            
-            $lat_ = round($request->lat, 3);
-            $lng_ = round($request->lng, 3);
-
-            //Buscar Tienda
-
-            $Store=null;
-            
-
-
             $Stores = Store::All()->where('state', 'ACTIVO');
-    
-            foreach ($Stores as $key => $value) {
-                $store_lat = round($value->lat, 3);
-                $store_lng = round($value->lng, 3);
+            $str;
+            foreach ($Stores as $item) {
+            
+                if (($item->lat==$request->lat) && ($item->lng==$request->lng)) {
 
-                if ($store_lat == $lat_ && $store_lng==$lng_) {
-                    $Store = $value;    
+                    $str=$item;
+
+                    $Product = Product::where('state', 'ACTIVO')
+                    ->where('barcode', $request->barcode)
+                    ->where('store_id', $item->id)
+                    ->with('store', 'subcategory')
+                    ->first();
+
+                    //Registro actividad
+                    $Activity = Activity::create([
+                        'user_id' => $request->user_id,
+                        'product_id' => $Product->id,
+                    ]);
+    
+                    return response()->json(['success' => true, 'msg' => 'Registro encontrado', 'obj' => $Product]);
+
                 }
             }
-
-            if ($Store != null) {
-
-
-
-
-                $Product = Product::where('state', 'ACTIVO')
-                ->where('barcode', $request->barcode)
-                ->where('store_id', $Store->id)
-                ->with('store', 'subcategory')
-                ->first();
-
-
-                //Registro actividad
-                $Activity = Activity::create([
-                    'user_id' => $request->user_id,
-                    'product_id' => $Product->id,
-                ]);
-
-// //===================================================================================
-// return response()->json(['success' => true, 'msg' => 'LLEGANDO', 'obj' => $Product, 'activity' => $Activity]);
-// //===================================================================================
-
-
-                return response()->json(['success' => true, 'msg' => 'Registro encontrado', 'obj' => $Product]);
-
-            }else {
-                return response()->json(['success' => false, 'msg' => 'No se encuntrar registros con la ubicación.']);
-            }
-
-        } else {
-            return response()->json(['success' => false, 'msg' => 'El código no esta registrado, intente de nuevo']);
-        }
-
-
+            return response()->json(['success' => false, 'msg' => 'No se encuntrar registros con la ubicación.']);
+            
     }
 }
